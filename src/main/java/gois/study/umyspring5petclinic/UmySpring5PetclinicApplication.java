f7705cf6092da546eca0313bbb4e24e113d642d9
Gustavo Gois <gustavo.gois@gmail.com>
2019-10-01 21:41:31 +0100

    Initial commit

---
+++ src/main/java/gois/study/umyspring5petclinic/UmySpring5PetclinicApplication.java
@@ -0,0 +1,13 @@
+package gois.study.umyspring5petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmySpring5PetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmySpring5PetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gois/study/umyspring5petclinic/UmySpring5PetclinicApplicationTests.java
@@ -0,0 +1,16 @@
+package gois.study.umyspring5petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UmySpring5PetclinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
