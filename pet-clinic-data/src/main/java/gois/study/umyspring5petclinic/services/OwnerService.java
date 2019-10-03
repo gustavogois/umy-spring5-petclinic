0af52028e0db1fc186c805f0e5322433dd0559a4
Gustavo Gois <gustavo.gois@gmail.com>
2019-10-03 23:15:38 +0100

    Refactor Service Interface to Common Base Interface - Closes #11

--- pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/services/OwnerService.java
@@ -2,15 +2,8 @@ package gois.study.umyspring5petclinic.services;
 
 import gois.study.umyspring5petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/services/VetService.java
@@ -2,13 +2,6 @@ package gois.study.umyspring5petclinic.services;
 
 import gois.study.umyspring5petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
