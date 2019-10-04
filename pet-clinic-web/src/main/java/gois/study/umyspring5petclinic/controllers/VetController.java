b8f2c14206a0f8a76d8703d731fd40390cf5856e
Gustavo Gois <gustavo.gois@gmail.com>
2019-10-04 14:57:22 +0100

    Create Vet Index Page and Controller, closes #7

---
+++ pet-clinic-web/src/main/java/gois/study/umyspring5petclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package gois.study.umyspring5petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+
+
+}
