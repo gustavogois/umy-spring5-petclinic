d7a449c3195b6361a700ba23b5cb28cfdf0bf7b2
Gustavo Gois <gustavo.gois@gmail.com>
2019-10-04 14:27:03 +0100

    Add Pet Clinic Index Page and Controller, closes #8

---
+++ pet-clinic-web/src/main/java/gois/study/umyspring5petclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package gois.study.umyspring5petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
