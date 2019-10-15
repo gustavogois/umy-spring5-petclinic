37eeeb2861765b7ebe882d87886726cc9f93619e
Gustavo Gois <gustavo.gois@gmail.com>
2019-10-15 17:10:23 +0100

    Create Visit Entity, closes #39

--- pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/model/Pet.java
@@ -1,23 +1,30 @@
 package gois.study.umyspring5petclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
 
     private String name;
+
     @ManyToOne
     @JoinColumn(name = "type_id")
+
     private PetType petType;
+
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
 
     public String getName() {
         return name;
@@ -49,5 +56,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
