f3496a132b97ddf738046637ee82f615ad9d9ff7
Gustavo Gois <gustavo.gois@gmail.com>
2019-10-15 16:08:04 +0100

    Convert Owners to JPA Entities, closes #37

--- pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/model/Person.java
@@ -1,5 +1,8 @@
 package gois.study.umyspring5petclinic.model;
 
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
     private String firstName;
