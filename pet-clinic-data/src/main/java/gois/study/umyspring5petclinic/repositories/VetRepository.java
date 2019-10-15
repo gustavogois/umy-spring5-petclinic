2d6ebac5c40c1ca1c6d440234684574f3196b3ba
Gustavo Gois <gustavo.gois@gmail.com>
2019-10-15 18:33:08 +0100

    Create Spring Data JPA Service for Vets, closes #42

--- pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/repositories/VetRepository.java
@@ -4,4 +4,5 @@ import gois.study.umyspring5petclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VetRepository extends CrudRepository<Vet, Long> {
+    Vet findByLasttName(String lastName);
 }
