a7f7481ea1944ac53d17add96cd73e352a029a6a
Gustavo Gois <gustavo.gois@gmail.com>
2019-10-16 18:11:20 +0100

    Add Project Lombok and Refactor

--- pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/model/Person.java
@@ -1,26 +1,17 @@
 package gois.study.umyspring5petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.MappedSuperclass;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
 
     private String firstName;
     private String lasttName;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLasttName() {
-        return lasttName;
-    }
-
-    public void setLasttName(String lasttName) {
-        this.lasttName = lasttName;
-    }
 }
--- pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/model/Speciality.java
@@ -1,19 +1,19 @@
 package gois.study.umyspring5petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
     private String description;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
