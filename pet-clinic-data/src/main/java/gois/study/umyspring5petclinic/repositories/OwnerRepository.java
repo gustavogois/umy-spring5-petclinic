3adcb0c2733fe37d0ad7207d941be40a047395f1
Gustavo Gois <gustavo.gois@gmail.com>
2019-10-25 17:51:01 +0100

    Implement Find Owner

--- pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/gois/study/umyspring5petclinic/repositories/OwnerRepository.java
@@ -3,6 +3,9 @@ package gois.study.umyspring5petclinic.repositories;
 import gois.study.umyspring5petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
